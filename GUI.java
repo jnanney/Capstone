d61ef7ff134c8b40cdae07b6b988c1f358db5b5b
Jonathan Nanney <jnanney1207@gmail.com>
2009-09-14 23:10:47 -0400

    I have created a very simple frame with one button that will open a file chooser box.

--- GUI.java
+++ GUI.java
@@ -6,9 +6,35 @@
 //Replace the * with the packages that I'm actually using once I know what 
 //I'm actually using
 import javax.swing.*;
-public class MainGUI
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Container;
+
+public class GUI
 {
    public static void main(String[] args)
    {
+      createGUI();
+   }
+
+   public static void createGUI()
+   {
+      final JFrame frame = new JFrame("File Encryption With OpenPGP");
+      Container contentPane = frame.getContentPane();
+      final JFileChooser chooser = new JFileChooser();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JButton addFileButton = new JButton("Add File");  
+      contentPane.add(addFileButton);
+
+      addFileButton.addActionListener(new ActionListener() 
+      {
+         public void actionPerformed(ActionEvent evt)
+         {
+            chooser.showOpenDialog(frame);
+         }
+      });
+
+      frame.pack();
+      frame.setVisible(true);
    }
 }
