1e9b11d23fa1ca27763c3829db8c8b3cf5ceb944
Jonathan Nanney <jnanney1207@gmail.com>
2009-09-14 21:30:56 -0400

    I added a few minor changes to the GUI file.

--- GUI.java
+++ GUI.java
@@ -1,3 +1,11 @@
+/**
+ * Contains the main window for the application
+ * Author - Jonathan Nanney
+ **/
+
+//Replace the * with the packages that I'm actually using once I know what 
+//I'm actually using
+import javax.swing.*;
 public class MainGUI
 {
    public static void main(String[] args)
