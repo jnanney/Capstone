a2929290464b09666e0ed5e0f55d5027167b8aa0
Jonathan Nanney <jnanney1207@gmail.com>
2009-09-17 17:32:52 -0400

    I now have it so that the user may select files and it will show up in the text area.

--- GUI.java
+++ GUI.java
@@ -5,13 +5,20 @@
 
 //Replace the * with the packages that I'm actually using once I know what 
 //I'm actually using
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.io.File;
 
 public class GUI
 {
+   private static final int TEXTAREA_ROWS=10;
+   private static final int TEXTAREA_COLS=10;
    public static void main(String[] args)
    {
       createGUI();
@@ -23,14 +30,23 @@ public class GUI
       Container contentPane = frame.getContentPane();
       final JFileChooser chooser = new JFileChooser();
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      final JTextArea fileText = new JTextArea(TEXTAREA_ROWS, TEXTAREA_COLS);
       JButton addFileButton = new JButton("Add File");  
-      contentPane.add(addFileButton);
+      JButton encryptButton = new JButton("Encrypt Files");
+      contentPane.add(fileText, BorderLayout.PAGE_START);
+      contentPane.add(addFileButton, BorderLayout.LINE_START);
+      contentPane.add(encryptButton, BorderLayout.LINE_END);
 
       addFileButton.addActionListener(new ActionListener() 
       {
          public void actionPerformed(ActionEvent evt)
          {
-            chooser.showOpenDialog(frame);
+            int returnValue = chooser.showOpenDialog(frame);
+            if (returnValue == JFileChooser.APPROVE_OPTION)
+            {
+               File selectedFile = chooser.getSelectedFile();
+               fileText.append(selectedFile.getName() + "\n");
+            }
          }
       });
 
