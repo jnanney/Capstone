fe7e300f6eda9505f507f4290bebe59e1f6d66fd
Jonathan Nanney <jnanney1207@gmail.com>
2009-09-21 16:57:10 -0400

    Improved the GUI a little.  Added a scrollbar and made it so that it shows which number file you are on.

--- GUI.java
+++ GUI.java
@@ -3,37 +3,47 @@
  * Author - Jonathan Nanney
  **/
 
-//Replace the * with the packages that I'm actually using once I know what 
-//I'm actually using
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JTextArea;
+import javax.swing.Popup;
+import javax.swing.JScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.io.File;
+import java.util.ArrayList;
+
 
 public class GUI
 {
    private static final int TEXTAREA_ROWS=10;
    private static final int TEXTAREA_COLS=10;
-   public static void main(String[] args)
+
+   public GUI()
    {
       createGUI();
    }
 
-   public static void createGUI()
+   public static void main(String[] args)
+   {
+      GUI gui = new GUI();
+   }
+
+   public void createGUI()
    {
       final JFrame frame = new JFrame("File Encryption With OpenPGP");
       Container contentPane = frame.getContentPane();
-      final JFileChooser chooser = new JFileChooser();
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       final JTextArea fileText = new JTextArea(TEXTAREA_ROWS, TEXTAREA_COLS);
       JButton addFileButton = new JButton("Add File");  
       JButton encryptButton = new JButton("Encrypt Files");
-      contentPane.add(fileText, BorderLayout.PAGE_START);
+      final ArrayList<File> fileList = new ArrayList<File>();
+      JScrollPane scrollingFileText = new JScrollPane(fileText);
+
+      contentPane.add(scrollingFileText, BorderLayout.PAGE_START);
       contentPane.add(addFileButton, BorderLayout.LINE_START);
       contentPane.add(encryptButton, BorderLayout.LINE_END);
 
@@ -41,12 +51,24 @@ public class GUI
       {
          public void actionPerformed(ActionEvent evt)
          {
+            JFileChooser chooser = new JFileChooser();
             int returnValue = chooser.showOpenDialog(frame);
             if (returnValue == JFileChooser.APPROVE_OPTION)
             {
                File selectedFile = chooser.getSelectedFile();
-               fileText.append(selectedFile.getName() + "\n");
+               fileList.add(selectedFile);
+               int num = fileList.size();
+               fileText.append(num + ") " + selectedFile.getName() + "\n");
+
             }
+         }
+      });
+
+      encryptButton.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent evt)
+         {
+            
          }
       });
 
