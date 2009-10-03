c6e8834c154217e05a83dcc2f9744d9baa887d1e
Jonathan Nanney <jnanney1207@gmail.com>
2009-10-02 23:20:13 -0400

    I worked on the RSA key some.  I've decided to use BigIntegers.  At some point I may decide to try to modify bigintegers to be unsigned.

--- RSAKey.java
+++ RSAKey.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+import java.math.BigInteger;
+
 public class RSAKey
 {
-   public RSAKey()
+   private BigInteger prime1;
+   private BigInteger prime2;
+   private BigInteger n;
+   private BigInteger encryptionExponent;
+
+   public RSAKey(int numBits)
    {
+      Random random = new Random();
+      int primeCertainty = 100;
+      prime1 = new BigInteger(numBits, primeCertainty, random);
+      prime2 = new BigInteger(numBits, primeCertainty, random);
+       
+   }
+   
+
+   public String toString()
+   {
+      return "Prime 1: " + prime1 + "\nPrime 2: " + prime2;
    }
 }
