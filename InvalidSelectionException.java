ec5ef3fc5f1fe83e2a6930b0127b0cd4771c5946
Jonathan Nanney <jnanney1207@gmail.com>
2009-10-23 16:21:20 -0400

    Forgot to add InvalidSelectionException file on my last commit.

---
+++ InvalidSelectionException.java
@@ -0,0 +1,12 @@
+public class InvalidSelectionException extends Exception
+{
+   public InvalidSelectionException()
+   {
+      super();
+   }
+
+   public InvalidSelectionException(String message)
+   {
+      super(message);
+   }
+}
