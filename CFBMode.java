9e175c090853acf11eec775557c3d1511600b455
Jonathan Nanney <jnanney1207@gmail.com>
2009-10-31 23:26:38 -0400

    Worked on CFBMode a tiny bit.

--- CFBMode.java
+++ CFBMode.java
@@ -4,13 +4,26 @@ public class CFBMode
    public static final int BLOCK_SIZE = 8;
 
    private TripleDESEncryption encryptor;
-
+   private long text;
    public CFBMode(long text)
    {
    }
 
-   public CFBMode(TripleDESEncryption encryptor)
+   public CFBMode(long text, TripleDESEncryption encryptor)
    {
       this.encryptor = encryptor;
+      this.text = text;
    }
+
+   public CFBMode(long text)
+   {
+      this.text = text;
+   }
+
+   public TripleDESEncryption getEncryptor()
+   {
+      return encryptor;
+   }
+
+
 }
