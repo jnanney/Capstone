125d7879d44ae819e75431e104df7f02fd229cfc
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-08 14:31:21 -0500

    Worked on changing RSA encryption from working on strings to working on BigIntegers/byte arrays.  I haven't tested it yet but I know it won't work when the message is longer than n.

--- RSAEncryption.java
+++ RSAEncryption.java
@@ -3,62 +3,36 @@ import java.util.ArrayList;
 import java.math.BigInteger;
 public class RSAEncryption 
 {
-   private String text;
    private RSABaseKey key;
    private BigInteger original;
+   int SIGN=1;
 
-   public RSAEncryption(String message, int keylength)
-   {
-      text = message;
-      key = new RSAPrivateKey(keylength);
-   }
+
    public RSAEncryption(byte[] data, RSABaseKey key)
    {
-      BigInteger original = new BigInteger(data);
+      BigInteger original = new BigInteger(SIGN, data);
       this.key = key;
    }
 
    public RSAEncryption(byte[] data, int keylength)
    {
-      BigInteger original = new BigInteger(data);
+      BigInteger original = new BigInteger(SIGN, data);
       this.key = new RSAPrivateKey(keylength);
    }
-
-   public RSAEncryption(String message, RSABaseKey key)
-   {
-      this.text = message;
-      this.key = key;
-   }
-   
-   /*public BigInteger encrypt()
-   {
-	   BigInteger n = key.getPrimeProduct();
-	   String numString = Common.makeNumberString(text);
-	   String splitText[] = Common.split(numString, n.toString().length() - 1);
-	   String cyphertext = "";
-	   for(String current : splitText)
-	   {
-		   BigInteger num = new BigInteger(current);
-		   cyphertext +=  num.modPow(key.getEncryptionExponent(), n);
-	   }
-	   return new BigInteger(cyphertext);
-   }*/
 
    public BigInteger encrypt()
    {
 	   BigInteger n = key.getPrimeProduct();
-	   String numString = original.toString();
-	   String splitText[] = Common.split(numString, n.toString().length() - 1);
-	   String cyphertext = "";
-	   for(String current : splitText)
-	   {
-		   BigInteger num = new BigInteger(current);
-		   cyphertext +=  num.modPow(key.getEncryptionExponent(), n);
-	   }
-	   return new BigInteger(cyphertext);
+      if(original.compareTo(n) > 0)
+      {
+         System.out.println("message too large.  Implement splitting");
+      }
+
+      BigInteger cyphertext = original.modPow(key.getEncryptionExponent(), n);
+	   return new cyphertext;
    }
    
-   public String decrypt()
+   public BigInteger decrypt()
    {
       if(!(key instanceof RSAPrivateKey))
       {
@@ -66,24 +40,14 @@ public class RSAEncryption
          System.exit(1);
       }
       RSAPrivateKey privateKey = (RSAPrivateKey) key;
-	   BigInteger n = privateKey.getPrimeProduct();
-	   String result = "";
-	   int length = privateKey.getPrimeProduct().toString().length();
-	   String splitText[] = Common.split(text, length);
-	   for(String current : splitText)
-	   {
-		   BigInteger temp = new BigInteger(current);
-		   temp = temp.modPow(privateKey.getDecryptionExponent(), n);
-		   result += temp.toString();
-	   }
-	   result = Common.addLeadingZeros(new BigInteger(result), Common.CHAR_SIZE);
-	   result = Common.makeCharString(result);
+	   BigInteger n = key.getPrimeProduct();
+      BigInteger result = temp.modPow(privateKey.getDecryptionExponent(), n);
 	   return result;
    }
 
-   public void switchText(String newText)
+   public void switchOriginal(BigInteger newText)
    {
-      this.text = newText;
+      this.original = newText;
    }
 
 }
