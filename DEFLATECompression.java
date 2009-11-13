27b3843ca49196eb0ece1202401f2143c8c93db1
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-12 21:32:18 -0500

    Added a method for getting unsigned byte values and added a test for it.

--- DEFLATECompression.java
+++ DEFLATECompression.java
@@ -3,4 +3,20 @@ public class DEFLATECompression
    public DEFLATECompression(byte[] data)
    {
    }
+   
+
+   public void weightBytes()
+   {
+      int[] weights = new int[255];
+      java.util.Arrays.fill(weights, 0);
+      for(int i = 0; i < data.length; i++)
+      {
+         weights[Common.byteValue(data[i])]++;
+      }
+   }
+
+   public void generateTree()
+   {
+   }
 }
+
--- UnitTests.java
+++ UnitTests.java
@@ -1,26 +1,34 @@
 import org.junit.Assert;
-
+import org.junit.Test;
 public class UnitTests
 {
-   @org.junit.Test public void basicDESTest() throws Exception
+   @Test public void basicDESTest() throws Exception
    {
       long original = 1234567;
       DESEncryption des = new DESEncryption(original);
       long encrypted = des.encrypt();
       DESEncryption desDecrypt = new DESEncryption(encrypted, des.getKey());
       long decrypted = desDecrypt.decrypt();
-      org.junit.Assert.assertTrue(encrypted != decrypted);
-      org.junit.Assert.assertTrue(decrypted == original);
+      Assert.assertTrue(encrypted != decrypted);
+      Assert.assertTrue(decrypted == original);
    }
 
-   @org.junit.Test public void basic3DESTest() throws Exception
+   @Test public void basic3DESTest() throws Exception
    {
       long original = 1234567;
       TripleDESEncryption des = new TripleDESEncryption(original);
       long encrypted = des.encrypt();
       des.changeOriginal(encrypted);
       long decrypted = des.decrypt();
-      org.junit.Assert.assertTrue(encrypted != decrypted);
-      org.junit.Assert.assertTrue(decrypted == original);
+      Assert.assertTrue(encrypted != decrypted);
+      Assert.assertTrue(decrypted == original);
    }
+
+   @Test public void byteValueTest()
+   {
+      int original = 255;
+      byte byteCopy = (byte) original;
+      Assert.assertTrue(Common.byteValue(byteCopy) == original);
+   }
+
 }
