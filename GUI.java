4af6dffe8ce9ebe27b9467e7a66af8c9524e731d
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-17 21:33:54 -0500

    Tried getting a GridBagLayout working to no avail.

--- GUI.java
+++ GUI.java
@@ -16,6 +16,8 @@ import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.io.File;
@@ -197,15 +199,28 @@ public class GUI
    private JPanel createRSAOptionPanel(final Container pane)
    {
       final JPanel panel = new JPanel();
-      panel.setLayout(new BorderLayout());
+      panel.setLayout(new GridBagLayout());
       JButton newKey = new JButton("Generate a new key");
       JButton existingKey = new JButton("Use an existing key");
       final JTextField keyLength = new JTextField("1024", 10);      
-      panel.add(newKey, BorderLayout.LINE_END);
-      panel.add(existingKey, BorderLayout.LINE_START);
-      panel.add(keyLength, BorderLayout.PAGE_START);
-      
+      /*panel.add(newKey);
+      panel.add(existingKey);
+      panel.add(keyLength);*/
 
+      GridBagConstraints constraints = new GridBagConstraints();
+      constraints.fill = constraints.HORIZONTAL;
+      /*constraints.gridx = 0;
+      constraints.gridy = 
+      constraints.weightx = 0.5;*/
+      constraints.anchor = constraints.FIRST_LINE_START;
+      panel.add(keyLength, constraints);
+
+      GridBagConstraints constraints2 = new GridBagConstraints();
+      constraints2.fill = constraints2.HORIZONTAL;
+      constraints2.gridx = 1;
+      constraints2.gridy = 0;
+      constraints2.weightx = 0.5;
+      panel.add(newKey, constraints2);
       existingKey.addActionListener(new ActionListener()
       {
          public void actionPerformed(ActionEvent evt)
