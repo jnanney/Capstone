aab6788acef96b03d76b3d67c64f499bac12e30d
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-17 20:22:32 -0500

    Commented RSAEncryption

--- RSAEncryption.java
+++ RSAEncryption.java
@@ -1,31 +1,57 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.math.BigInteger;
+/**
+ * This class does RSA encryption/decryption
+ * @author Jonathan Nanney
+ * */
 public class RSAEncryption 
 {
+   /** The key to encrypt/decrypt with */
    private RSABaseKey key;
+   /** The data to encrypt/decrypt */
    private BigInteger original;
-   int SIGN=1;
-
-
+   /** Makes BigIntegers positive */
+   private int SIGN=1;
+   
+   /**
+    * Constructor that takes the data to encrypt/decrypt and a RSA Key.
+    * @param data - the data to encrypt/decrypt
+    * @param key - the RSA key to use for encryption/decryption
+    * */
    public RSAEncryption(byte[] data, RSABaseKey key)
    {
       original = new BigInteger(SIGN, data);
       this.key = key;
    }
 
+   /**
+    * Constructor that takes the data to encrypt/decrypt and a RSA key.
+    * @param original - the data to encrypt/decrypt
+    * @param key - the RSA key to use for encryption/decryption
+    * */
    public RSAEncryption(BigInteger original, RSABaseKey key)
    {
       this.original = original;
       this.key = key;
    }
-
+   
+   /**
+    * Constructor that takes the data to encrypt/decrypt and will generate a
+    * new key of the given length.
+    * @param data - the data to encrypt/decrypt
+    * @param keylength - the length of the key (in bits) to generate 
+    * */
    public RSAEncryption(byte[] data, int keylength)
    {
       original = new BigInteger(SIGN, data);
       this.key = new RSAPrivateKey(keylength);
    }
-
+   
+   /**
+    * Encrypts the data
+    * @return the encrypted data
+    * */
    public BigInteger encrypt()
    {
 	   BigInteger n = key.getPrimeProduct();
@@ -38,6 +64,10 @@ public class RSAEncryption
 	   return cyphertext;
    }
    
+   /**
+    * Decrypts the data
+    * @return the decrypted data
+    * */
    public BigInteger decrypt()
    {
       if(!(key instanceof RSAPrivateKey))
@@ -51,7 +81,11 @@ public class RSAEncryption
       BigInteger result = original.modPow(privateKey.getDecryptionExponent(), n);
 	   return result;
    }
-
+   
+   /**
+    * Allows you to switch the data to encrypt/decrypt
+    * @param newText - the new data to encrypt/decrypt
+    * */
    public void switchOriginal(BigInteger newText)
    {
       this.original = newText;
