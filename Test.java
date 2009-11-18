4288620da9c2f2223a1f153dee4e54e96ba60806
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-17 20:27:42 -0500

    Modified the Test class for submission

--- Test.java
+++ Test.java
@@ -5,20 +5,20 @@ public class Test
 {
    public static void main(String[] args) throws Exception
    {
-      if(args.length != 2)
+      if(args.length != 3)
       {
-         System.err.println("Usage: java Test <encrypted file> <decrypted file>");
+         System.err.println("Usage: java Test <original> <encrypted file> <decrypted file>");
          System.exit(1);
       }
       RSAPrivateKey key = new RSAPrivateKey(1024);
-      FileEncryptor encryptor = new FileEncryptor(new File("hello"), key);
-      encryptor.write(new File(args[0]));
+      FileEncryptor encryptor = new FileEncryptor(new File(args[0]), key);
+      encryptor.write(new File(args[1]));
 
-      FileDecryptor decryptor = new FileDecryptor(new File(args[0]), key);
-      decryptor.write(new File(args[1]));
+      FileDecryptor decryptor = new FileDecryptor(new File(args[1]), key);
+      decryptor.write(new File(args[2]));
 
       Runtime rt = Runtime.getRuntime();
-      Process p = rt.exec("diff hello " + args[1]);
+      Process p = rt.exec("diff " + args[0] + " " + args[1]);
       
       //if(p.waitFor() != 0)
       //{
