b994f302a36fd191ce67cbb0358874881f128b65
Jonathan Nanney <jnanney1207@gmail.com>
2009-11-28 20:36:27 -0500

    Made it so that the SignaturePacket writes the data and gives a correct length.

--- SignaturePacket.java
+++ SignaturePacket.java
@@ -1,5 +1,9 @@
 import java.io.OutputStream;
 import java.math.BigInteger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SignaturePacket implements PacketSpecificInterface
 {
@@ -7,24 +11,30 @@ public class SignaturePacket implements PacketSpecificInterface
    private static final byte TYPE = 0x0;
    private static final byte PUBLIC_KEY_ALGORITHM = OpenPGP.RSA_CONSTANT;
    private static final byte HASH_ALGORITHM = OpenPGP.SHA1_CONSTANT; 
-   private static final BigInteger signature;
+   private byte[] signature;
 
    public SignaturePacket(byte[] data)
    {
    }
    
-   public SignaturePacket(File input)
+   public SignaturePacket(File input, RSAPrivateKey key) throws IOException, 
+      FileNotFoundException
    {
       byte[] fileData = Common.readAllData(new FileInputStream(input));
-      SHA1.hash(fileData);
+      byte[] hashedData = SHA1.hash(fileData);
+      RSAEncryption rsa = new RSAEncryption(hashedData, key);
+      signature = rsa.decrypt().toByteArray();
    }
 
    public int getBodyLength()
    {
-      return 0;
+      return 1 + 1 + 1 + 1 + signature.length;
    }
 
-   public void write(OutputStream out)
+   public void write(OutputStream out) throws IOException
    {
+      out.write(new byte[]{VERSION, TYPE, PUBLIC_KEY_ALGORITHM, 
+                           HASH_ALGORITHM});
+      out.write(signature);
    }
 }
